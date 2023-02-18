1bdf86e74964a467cbcfe5fd22c508311a244386
Valeriy Fateev <Valerka9602@mail.ru>
2023-02-18 15:43:26 +0800

    Решение задачи 8

--- Ex08/Program.cs
+++ Ex08/Program.cs
@@ -3,3 +3,46 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+
+int i;
+string result = "Все чётные числа от 1 до N: ";
+
+Console.Write("Введиче число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 1) // как я понял по условию задачи, чётные числа начинаем отсчитывать от +1 и поэтому если N положитьельный, то результат начинается от 2, а если отрицательный — от 0.
+{
+    for (i = 0; i >= num; i = i - 2)
+    {
+        if (i % 2 == 0)
+        {
+            if (i > num && i != num + 1)
+            {
+                result = result + i + ", ";
+            }
+            else
+            {
+                result = result + i + ".";
+            }
+        }
+    }
+}
+else
+{
+    for (i = 2; i <= num; i = i + 2)
+    {
+        if (i % 2 == 0)
+        {
+            if (i < num && i != num - 1)
+            {
+                result = result + i + ", ";
+            }
+            else
+            {
+                result = result + i + ".";
+            }
+        }
+    }
+}
+
+if (num == 1) result = "Чётные числа от 1 до N не найдены";
+Console.WriteLine(result);
